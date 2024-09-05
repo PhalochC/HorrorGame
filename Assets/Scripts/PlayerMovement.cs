804569fd62d43f16d0d1b85e43c9b7705a4be32b
Phaloch Chommai <2310511105010@live4.utcc.ac.th>
2024-09-05 14:30:53 +0700

    update

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float turnspeed = 0f;
+
+    Vector3 movement;
+    Quaternion rotation = Quaternion.identity;
+
+    Rigidbody rb;
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        movement.Set(horizontal, 0f, vertical);
+        movement.Normalize();
+
+        bool hasInputHorizontal = !Mathf.Approximately (horizontal, 0f);
+        bool hasInputVertical = !Mathf.Approximately(vertical, 0f);
+        bool isWalking = hasInputHorizontal || hasInputVertical;
+        if (isWalking)
+        {
+            animator.SetBool("IsWalking", true);
+        } else if (!isWalking)
+        {
+            animator.SetBool("IsWalking", false);
+        }
+        
+
+        Vector3 desireForward = Vector3.RotateTowards(transform.forward, movement, turnspeed * Time.deltaTime, 0f);
+        rotation = Quaternion.LookRotation(desireForward);
+    }
+
+    private void OnAnimatorMove()
+    {
+        rb.MovePosition(rb.position + movement * animator.deltaPosition.magnitude);
+        rb.MoveRotation(rotation);
+    }
+}
